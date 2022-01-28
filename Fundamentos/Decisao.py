ebcb4ba10d4aa49d331d3f204038f8876ddee579
DjCod3r <Djonatanschvambach@gmail.com>
2022-01-28 11:51:18 -0300

    lacos repeticao

---
+++ Fundamentos/Decisao.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome")
+idade = int(input("Digite sua idade"))
+if idade >= 65:
+    prioritario ="Sim"
+else: prioritario ="Nao"
+print("O Paciente" , nome , " com idade de ", idade , " prioritário ? " , prioritario )
+
+
