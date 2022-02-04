a190adad54c51231fa7a8379456aa073ef28fa20
DjCod3r <Djonatanschvambach@gmail.com>
2022-02-04 13:21:37 -0300

    Format

--- Logica/FormatarString.py
+++ Logica/FormatarString.py
@@ -7,4 +7,7 @@ imc = peso / altura ** 2
 #Utilização de Interpolação no Python
 #formatação de 2 casas decimais
 print(f'{nome}{sobrenome} idade {idade} IMC {imc:.2f}')
-print('Nome {}{} idade {}'.format(nome , sobrenome, idade))
+print('Nome {}{} idade {} {:.2f}'.format(nome , sobrenome, idade, imc))
+
+anoNascimento = 2022 - idade
+print(anoNascimento)
