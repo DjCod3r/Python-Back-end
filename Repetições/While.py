9d62729501e533d79569fca1e6f6734b07384d7d
DjCod3r <Djonatanschvambach@gmail.com>
2022-02-06 11:01:31 -0300

    Create While.py

---
+++ Repetições/While.py
@@ -0,0 +1,24 @@
+'''nome = ''
+while nome != 'Djon':
+    nome = input('Qual seu nome?')
+    print(f'ola {nome}')
+print('Acertou o nome')'''
+x = 0
+
+while x < 10:
+    j = 0
+    while j <= 9:
+        k = 0
+        while k <= 9:
+            print(f'{x}{j}{k}')
+            k += 1
+        j += 1
+    x += 1
+
+
+
+
+
+
+
+
