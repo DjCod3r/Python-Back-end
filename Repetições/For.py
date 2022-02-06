da71772ec06a36dd3d93b94ea83c227502e9e3fd
DjCod3r <Djonatanschvambach@gmail.com>
2022-02-06 16:54:14 -0300

    Update For.py

--- Repetições/For.py
+++ Repetições/For.py
@@ -9,3 +9,16 @@ for n in range(10):
 
 for n in range(0 , 11 , 2): #inicio , fim , step
     print(n)
+
+for n in range(100):
+    if n % 5 == 0:
+        print(n)
+
+texto = 'python'
+novastring = ''
+for letra in texto:
+    if letra == 'p':
+        novastring += letra.upper()
+    else:
+        novastring += letra
+print(novastring)
