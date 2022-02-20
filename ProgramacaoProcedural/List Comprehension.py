3a44f4fe653bef0b59a427a47d373f899a188c4d
Djonatan Schvambach <Djonatanschvambach@gmail.com>
2022-02-20 02:07:13 -0300

    Update List Comprehension.py

---
+++ ProgramacaoProcedural/List Comprehension.py
@@ -0,0 +1,22 @@
+listC1 = [1, 2, 3, 4, 5]
+listC2 = [6, 7, 8, 9, 10]
+
+
+
+receptor = [x for x in listC1 if x > 3]
+print(receptor)
+print([x for x in listC1])
+print([x for x in listC1])
+
+
+
+
+a = [(x, y) for x in listC1 for y in range(2)]
+
+print("Este e o valor" + str([valor ** 2 for valor in listC1]))
+print(a)
+
+
+l5 = ['Djonatan', 'Schvambach', 'Python', 'Java', 'C++']
+
+print([ valor.replace('a', '@') for valor in l5])
