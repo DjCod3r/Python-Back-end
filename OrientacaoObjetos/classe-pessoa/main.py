a4a5733ea1c3a701c412705fcdbc48d2ec6910a5
Djonatan Schvambach <Djonatanschvambach@gmail.com>
2022-02-21 11:32:19 -0300

    OOP

---
+++ OrientacaoObjetos/classe-pessoa/main.py
@@ -0,0 +1,6 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('João', 20)
+p2 = Pessoa('Maria', 25)
+p1.falar()
+p2.falar()
---
+++ OrientacaoObjetos/classe-pessoa/pessoa.py
@@ -0,0 +1,6 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+    def falar(self):
+        print('Olá, meu nome é {} e tenho {} anos.'.format(self.nome, self.idade))
